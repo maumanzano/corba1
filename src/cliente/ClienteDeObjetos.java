a949027c2aaa0a9879a53e315f985c329cc29bc3
philipretl <philipreaver@gmail.com>
2018-05-15 08:59:23 -0500

    Cliente enviado para pruebas

--- src/cliente/ClienteDeObjetos.java
+++ src/cliente/ClienteDeObjetos.java
@@ -9,7 +9,8 @@ import sop_corba.*;
 public class ClienteDeObjetos
 {
   //*** Atributo estático ***
-  static xxxxxx ref;
+  static solucionOperations ref;
+  static boolean flag=false;
 
   public static void main(String args[])
     {
@@ -24,18 +25,167 @@ public class ClienteDeObjetos
  
         // *** Resuelve la referencia del objeto en el N_S ***
         String name = "xxx";
-        ref = xxxxHelper.narrow(ncRef.resolve_str(name));
+        ref = solucionHelper.narrow(ncRef.resolve_str(name));
 
         System.out.println("Obtenido el manejador sobre el servidor de objetos: " +ref);
         
-        ref.xxxxxxMetodoRemoto();
         
+        menuPrincipal();
 
 	} catch (Exception e) {
           System.out.println("ERROR : " + e) ;
 	  e.printStackTrace(System.out);
 	  }
     }
+    
+    public static void menuPrincipal(){
+    int opcion = 0;
+        int jugada=0;
+        do
+        {
+            //ref.();
+            System.out.println("==Menu==");
+            System.out.println("1. Comenzar Juego");			
+            System.out.println("2. Adivinar letra");
+            System.out.println("3. Adivinar palabra");
+            System.out.println("4. Consultar longitud de palabra");
+            System.out.println("5. Consultar numero de intentos ");
+            System.out.println("6. Mostrar palabra");
+            System.out.println("0. Para salir...");
+
+            opcion = UtilidadesConsola.leerEntero();
+
+            switch(opcion)
+            {       case 0:
+                        System.out.println("Saliendo del juego...");
+                        System.exit(0);
+                        break;
+                        
+                    case 1:
+                        if(flag=false){
+                            iniciarJuego();
+                        }else{
+                            System.out.println("EL juego ya esta iniciado desea comenzar uno nuevo?");
+                            System.out.println("1. Para confirmar");
+                            System.out.println("0. para cancelar");
+                            opcion = UtilidadesConsola.leerEntero();
+
+                            if(opcion==1) iniciarJuego();
+                        }
+                        break;
+                    case 2:
+                        adivinarLetra();
+                        break;	
+                    case 3:
+                        adivinarPalabra();
+                        break;	
+                    
+                    case 4:
+                        longitudPalabra();
+                        break;
+                        
+                    case 5:
+                        numeroIntentos();
+                        break;
+                    case 6:
+                        mostrarPalabra();
+                        break;
+                    default:
+                            System.out.println("Opción incorrecta");
+            }
+
+        }while(opcion != 3);
+    }
+    
+    public static void iniciarJuego(){
+        ref.comenzarJuego();
+        flag=true;
+        System.out.println("Juego iniciado");
+    
+    }
+    
+    public static void adivinarLetra(){
+        String letra="";
+        
+        if(!flag){
+            System.out.println("Error primero debes iniciar un juego");
+        
+        }else{
+            System.out.println("Adivinar Letra");
+        
+            System.out.println("Digite una letra");
+
+            letra=UtilidadesConsola.leerCadena();
+
+            if(letra.length()>1){
+                System.out.println("Error solo puede digitar una letra");
+            }else{
+
+                System.out.println("Palabra: " + ref.adivinarLetra(letra.charAt(0)));
+            }
+        }    
+    }
+    
+    public static void adivinarPalabra(){
+        String palabra="";
+        boolean flag=false;
+        
+        if(!flag){
+            System.out.println("Error primero debes iniciar un juego");
+        
+        }else{
+            System.out.println("Adivinar Palabra");
+            System.out.println("Digite la palabra");
+
+            palabra=UtilidadesConsola.leerCadena();
+
+            if(palabra.length()==0){
+                System.out.println("La palabra que envio es vacia");
+            }else{    
+                flag=ref.adivinarPalabra(palabra);
+            }
+
+            if(flag){
+                System.out.println("Congrats adivino la palabra");
+                System.out.println("Fin del juego");
+                flag=false;
+            }
+        }
+    }
+    
+    public static void longitudPalabra(){
+        if(!flag){
+            System.out.println("Error primero debes iniciar un juego");
+        
+        }else{
+            System.out.println("La longitud de la palabra es:" + ref.longitudPalabra());
+        }
+    
+    
+    }
+    
+    public static void numeroIntentos(){
+        if(!flag){
+            System.out.println("Error primero debes iniciar un juego");
+        
+        }else{
+            System.out.println("Numero de intentos: " + ref.numeroIntentos());
+        }
+    
+    
+    }
+    
+    public static void mostrarPalabra(){
+        if(!flag){
+            System.out.println("Error primero debes iniciar un juego");
+        
+        }else{
+            System.out.println("La palabra era: " + ref.mostrarPalabras());
+            System.out.println("Usted perdio Fin del juego");
+            flag=false;
+        }
+    }
+    
 
 }
 
